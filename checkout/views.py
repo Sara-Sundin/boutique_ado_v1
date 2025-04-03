2089e1b1c68dc0fc4940fcb201ce10fc4168564e
Sara Sundin <sarasundin@rocketmail.com>
2025-04-03 09:21:41 +0200

    add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R9h08CTmqXXhmcza7n6e7Nct6cThiso4dpnVi9tUgwxIo6uMvlpG9V1Z89bgbgrCMLKICUOTR1TtMIerRtw1Q9o006kaLYgOZ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
